856e913f55f4ca3b42ce74cb7a74695304bdacec
Daniel Apodaca <apodacaduron@gmail.com>
2023-12-13 15:38:33 -0600

    use vorms and zod for form validation

--- src/pages/auth/SignIn.vue
+++ src/pages/auth/SignIn.vue
@@ -1,51 +1,55 @@
 <script setup lang="ts">
-import { supabase } from "@/config/supabase";
 import { AuthLayout } from "@/features/auth";
-import { InputGroup, VanillaInput, Button } from "@flavorly/vanilla-components";
+import { InputGroup, Input, Button } from "@flavorly/vanilla-components";
+import { useForm } from "@vorms/core";
+import { zodResolver } from "@vorms/resolvers/zod";
+import z from "zod";
 
-import { useField, useForm } from "vee-validate";
-import { toTypedSchema } from "@vee-validate/zod";
-import * as zod from "zod";
-
-const componentName = "sign-in";
-
-const validationSchema = toTypedSchema(
-  zod.object({
-    email: zod
-      .string()
-      .min(1, { message: "This is required" })
-      .email({ message: "Must be a valid email" }),
-    password: zod.string().min(1, { message: "This is required" }),
-  })
-);
-
-const { handleSubmit, errors } = useForm({
-  validationSchema,
+const { register, handleSubmit, errors } = useForm({
+  initialValues: {
+    email: "",
+    password: "",
+  },
+  validate: zodResolver(
+    z.object({
+      email: z
+        .string()
+        .min(1, "Email is required!")
+        .email("Must be a valid email"),
+      password: z.string().min(1, "Password is required!"),
+    })
+  ),
+  onSubmit(formValues) {
+    console.log(formValues, errors);
+  },
 });
-
-const onSubmit = handleSubmit(async (formValues) => {
-  const { data, error } = await supabase.auth.signInWithPassword({
-    email: formValues.email,
-    password: formValues.password,
-  });
-});
+const { value: email } = register("email");
+const { value: password } = register("password");
 </script>
 
 <template>
   <AuthLayout>
-    <form @submit="onSubmit" class="w-full max-w-md mx-auto">
+    <form @submit="handleSubmit" class="w-full max-w-md mx-auto">
       <InputGroup label="Email" name="email">
-        <VanillaInput name="email" placeholder="Your email" type="email" />
+        <Input
+          name="email"
+          placeholder="Your email"
+          type="email"
+          v-model="email"
+          :errors="errors.email"
+        />
       </InputGroup>
       <InputGroup label="Password" name="password">
-        <VanillaInput
+        <Input
           name="password"
           placeholder="Your password"
           type="password"
+          v-model="password"
+          :errors="errors.password"
         />
       </InputGroup>
       <InputGroup>
-        <Button label="Submit" variant="primary" />
+        <Button label="Submit" variant="primary" type="submit" />
       </InputGroup>
       <InputGroup>
         <div class="text-center">
