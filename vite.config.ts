86c2b0773c413448188ffff37c8215b3d517dd55
Daniel Apodaca <apodacaduron@gmail.com>
2024-02-26 22:23:31 -0600

    pwa improvements

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,40 @@
 import { fileURLToPath, URL } from "url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: [
+        "favicon.ico",
+        "apple-touch-icon.png",
+        "inventario-facil.svg",
+      ],
+      manifest: {
+        name: "Inventario fácil",
+        short_name: "Inventario fácil",
+        description:
+          "Gestiona fácilmente tu inventario, controla tus ventas, clientes y productos con nuestra plataforma intuitiva y poderosa. ¡Empieza hoy mismo!",
+        theme_color: "#000000",
+        icons: [
+          {
+            src: "android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: [
       {
