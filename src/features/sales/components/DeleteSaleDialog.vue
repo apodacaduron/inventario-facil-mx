26da2c61729c79e3eaa269066534fef974ba5f1e
Daniel Apodaca <apodacaduron@gmail.com>
2024-03-25 11:51:49 -0600

    update dialogs to be drawers on mobile

--- src/features/sales/components/DeleteSaleDialog.vue
+++ src/features/sales/components/DeleteSaleDialog.vue
@@ -7,8 +7,15 @@ import {
   DialogFooter,
   DialogHeader,
   DialogTitle,
+  Drawer,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
 } from "@/components/ui";
 import { Sale } from "../composables";
+import { useMediaQuery } from "@vueuse/core";
 
 type DeleteSaleDialogProps = {
   isLoading?: boolean;
@@ -20,10 +27,12 @@ defineProps<DeleteSaleDialogProps>();
 const emit = defineEmits<{
   (e: "confirmDelete", formValues: Sale | null): void;
 }>();
+
+const isDesktop = useMediaQuery("(min-width: 768px)");
 </script>
 
 <template>
-  <Dialog v-model:open="openModel">
+  <Dialog v-if="isDesktop" v-model:open="openModel">
     <DialogContent class="sm:max-w-[425px]">
       <DialogHeader>
         <DialogTitle>Eliminar venta {{ sale?.id }}</DialogTitle>
@@ -52,4 +61,36 @@ const emit = defineEmits<{
       </DialogFooter>
     </DialogContent>
   </Dialog>
+
+  <Drawer v-else v-model:open="openModel">
+    <DrawerContent>
+      <div class="mx-auto w-full max-w-sm mt-8 mb-16">
+        <DrawerHeader>
+          <DrawerTitle>Eliminar venta {{ sale?.id }}</DrawerTitle>
+          <DrawerDescription>
+            Esta acción eliminará permanentemente esta venta. ¿Estás seguro de
+            que deseas proceder con la eliminación?
+          </DrawerDescription>
+        </DrawerHeader>
+        <DrawerFooter>
+          <Button
+            :disabled="isLoading"
+            type="button"
+            variant="destructive"
+            @click="$emit('confirmDelete', sale)"
+          >
+            Si, eliminar
+          </Button>
+          <Button
+            :disabled="isLoading"
+            type="button"
+            variant="secondary"
+            @click="openModel = false"
+          >
+            Cancelar
+          </Button>
+        </DrawerFooter>
+      </div>
+    </DrawerContent>
+  </Drawer>
 </template>
