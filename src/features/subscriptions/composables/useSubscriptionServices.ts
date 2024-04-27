69225686a5478ae7408dc1abb1fb81594d5ffcd9
Daniel Apodaca <daniela@zoomcatalog.com>
2024-04-27 10:53:04 -0600

    remove console log

--- src/features/subscriptions/composables/useSubscriptionServices.ts
+++ src/features/subscriptions/composables/useSubscriptionServices.ts
@@ -42,7 +42,6 @@ export const subscriptionServicesTypeguards = {
   isSubscription(
     maybeSubscription: unknown
   ): maybeSubscription is Subscription {
-    console.log('maybeSubscription', maybeSubscription)
     return (
       maybeSubscription !== null &&
       typeof maybeSubscription === 'object' &&
