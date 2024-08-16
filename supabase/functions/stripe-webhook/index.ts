18a0186befb659f8c6fef09b0ab0664a3a672196
Daniel Apodaca <47479471+apodacaduron@users.noreply.github.com>
2024-08-16 14:41:10 -0600

    update to stripe function to support trial

--- supabase/functions/stripe-webhook/index.ts
+++ supabase/functions/stripe-webhook/index.ts
@@ -1,26 +1,26 @@
-import Stripe from 'npm:stripe@^11.16';
-import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
+import Stripe from "npm:stripe@^11.16";
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
-const stripe = new Stripe(Deno.env.get('STRIPE_API_KEY') as string, {
-  apiVersion: '2024-04-10',
+const stripe = new Stripe(Deno.env.get("STRIPE_API_KEY") as string, {
+  apiVersion: "2024-04-10",
   httpClient: Stripe.createFetchHttpClient(),
 });
 
-const supabaseUrl = Deno.env.get('SUPABASE_URL')!;
-const supabaseKey = Deno.env.get('SUPABASE_SERVICE_ROLE_KEY')!;
+const supabaseUrl = Deno.env.get("SUPABASE_URL")!;
+const supabaseKey = Deno.env.get("SUPABASE_SERVICE_ROLE_KEY")!;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 const cryptoProvider = Stripe.createSubtleCryptoProvider();
 
 Deno.serve(async (request) => {
-  const signature = request.headers.get('Stripe-Signature');
+  const signature = request.headers.get("Stripe-Signature");
   const body = await request.text();
   let receivedEvent;
   try {
     receivedEvent = await stripe.webhooks.constructEventAsync(
       body,
       signature!,
-      Deno.env.get('STRIPE_WEBHOOK_SIGNING_SECRET')!,
+      Deno.env.get("STRIPE_WEBHOOK_SIGNING_SECRET")!,
       undefined,
       cryptoProvider
     );
@@ -34,180 +34,152 @@ Deno.serve(async (request) => {
   );
 
   switch (receivedEvent.type) {
-    case 'invoice.payment_failed': {
-      const invoice = receivedEvent.data.object;
+    case "customer.subscription.created": {
+      const subscription = receivedEvent.data.object;
+
       const user = await supabase
-        .from('users')
-        .select('id')
-        .eq('stripe_customer_id', invoice.customer)
+        .from("users")
+        .select("id")
+        .eq("stripe_customer_id", subscription.customer)
         .single();
-      const freemiumPlanResponse = await supabase
-        .from('plans')
-        .select('*')
-        .eq('name', 'freemium')
+
+      const planResponse = await supabase
+        .from("plans")
+        .select("*")
+        .eq("stripe_price_id", subscription.items.data[0].price.id)
         .single();
-      await supabase
-        .from('subscriptions')
-        .update({
-          user_id: user.data.id,
-          plan_id: freemiumPlanResponse.data.id,
-          start_date: new Date().toISOString(),
-          status: 'active',
-        })
-        .eq('user_id', user.data.id);
-      await supabase
-        .from('i_organizations')
-        .update({
-          plan_id: freemiumPlanResponse.data.id,
-        })
-        .eq('user_id', user.data.id);
+
+      await supabase.from("subscriptions").insert({
+        user_id: user.data.id,
+        plan_id: planResponse.data.id,
+        start_date: new Date(subscription.current_period_start * 1000).toISOString(),
+        end_date: new Date(subscription.current_period_end * 1000).toISOString(),
+        status: subscription.status,
+        stripe_subscription_id: subscription.id,
+      });
+
+      await supabase.from("i_organizations").update({
+        plan_id: planResponse.data.id,
+      }).eq("user_id", user.data.id);
+
       break;
     }
-    case 'invoice.paid': {
+    case "customer.subscription.updated": {
+      const subscription = receivedEvent.data.object;
+
+      await supabase.from("subscriptions").update({
+        start_date: new Date(subscription.current_period_start * 1000).toISOString(),
+        end_date: new Date(subscription.current_period_end * 1000).toISOString(),
+        status: subscription.status,
+      }).eq("stripe_subscription_id", subscription.id);
+
+      break;
+    }
+    case "customer.subscription.deleted": {
+      const subscription = receivedEvent.data.object;
+      const user = await supabase
+        .from("users")
+        .select("id")
+        .eq("stripe_customer_id", subscription.customer)
+        .single();
+
+      const freemiumPlanResponse = await supabase
+        .from("plans")
+        .select("*")
+        .eq("name", "freemium")
+        .single();
+
+      await supabase.from("subscriptions").update({
+        plan_id: freemiumPlanResponse.data.id,
+        start_date: new Date().toISOString(),
+        end_date: null,
+        status: "canceled",
+        stripe_subscription_id: null,
+      }).eq("user_id", user.data.id);
+
+      await supabase.from("i_organizations").update({
+        plan_id: freemiumPlanResponse.data.id,
+      }).eq("user_id", user.data.id);
+
+      break;
+    }
+    case "invoice.payment_succeeded": {
       const invoice = receivedEvent.data.object;
+
       const subscription = await stripe.subscriptions.retrieve(
         invoice.subscription
       );
+
       const user = await supabase
-        .from('users')
-        .select('id')
-        .eq('stripe_customer_id', invoice.customer)
-        .single();
-      const premiumPlanResponse = await supabase
-        .from('plans')
-        .select('*')
-        .eq('name', 'premium')
+        .from("users")
+        .select("id")
+        .eq("stripe_customer_id", invoice.customer)
         .single();
 
-      const start_date = subscription.trial_start
-        ? new Date(subscription.trial_start * 1000).toISOString()
-        : new Date(subscription.current_period_start * 1000).toISOString();
+      await supabase.from("subscriptions").update({
+        status: subscription.status,
+      }).eq("user_id", user.data.id);
 
-      const end_date = subscription.trial_end
-        ? new Date(subscription.trial_end * 1000).toISOString()
-        : new Date(subscription.current_period_end * 1000).toISOString();
+      // Optional: Notify the user or take any action on payment success
 
-      await supabase
-        .from('subscriptions')
-        .update({
-          user_id: user.data.id,
-          plan_id: premiumPlanResponse.data.id,
-          start_date,
-          end_date,
-          status: subscription.status,
-          stripe_subscription_id: subscription.id,
-          stripe_invoice_id: subscription.latest_invoice,
-        })
-        .eq('user_id', user.data.id);
-      await supabase
-        .from('i_organizations')
-        .update({
-          plan_id: premiumPlanResponse.data.id,
-        })
-        .eq('user_id', user.data.id);
       break;
     }
-    case 'customer.subscription.updated': {
-      const subscription = receivedEvent.data.object;
-
-      const start_date = subscription.trial_start
-        ? new Date(subscription.trial_start * 1000).toISOString()
-        : new Date(subscription.current_period_start * 1000).toISOString();
-
-      const end_date = subscription.trial_end
-        ? new Date(subscription.trial_end * 1000).toISOString()
-        : new Date(subscription.current_period_end * 1000).toISOString();
-
-      await supabase
-        .from('subscriptions')
-        .update({
-          start_date,
-          end_date,
-          status: subscription.status,
-          stripe_subscription_id: subscription.id,
-          stripe_invoice_id: subscription.latest_invoice,
-        })
-        .eq('stripe_subscription_id', subscription.id);
-      break;
-    }
-    case 'customer.subscription.deleted': {
-      const subscription = receivedEvent.data.object;
-      const user = await supabase
-        .from('users')
-        .select('id')
-        .eq('stripe_customer_id', subscription.customer)
-        .single();
-      const freemiumPlanResponse = await supabase
-        .from('plans')
-        .select('*')
-        .eq('name', 'freemium')
-        .single();
-      await supabase
-        .from('subscriptions')
-        .update({
-          user_id: user.data.id,
-          plan_id: freemiumPlanResponse.data.id,
-          start_date: new Date().toISOString(),
-          end_date: null,
-          status: 'active',
-          stripe_subscription_id: null,
-          stripe_invoice_id: null,
-        })
-        .eq('user_id', user.data.id);
-      await supabase
-        .from('i_organizations')
-        .update({
-          plan_id: freemiumPlanResponse.data.id,
-        })
-        .eq('user_id', user.data.id);
-      break;
-    }
-    case 'price.created': {
+    case "price.created": {
       const price = receivedEvent.data.object;
+
       await supabase
-        .from('plans')
+        .from("plans")
         .update({
           currency: price.currency,
           price: price.unit_amount_decimal,
           stripe_price_id: price.id,
         })
-        .eq('stripe_product_id', price.product);
+        .eq("stripe_product_id", price.product);
+
       break;
     }
-    case 'price.updated': {
+    case "price.updated": {
       const price = receivedEvent.data.object;
+
       await supabase
-        .from('plans')
+        .from("plans")
         .update({
           currency: price.currency,
           price: price.unit_amount_decimal,
         })
-        .eq('stripe_price_id', price.id);
+        .eq("stripe_price_id", price.id);
+
       break;
     }
-    case 'product.created': {
+    case "product.created": {
       const product = receivedEvent.data.object;
-      await supabase.from('plans').insert({
+
+      await supabase.from("plans").insert({
         name: product.name,
         description: product.description,
         stripe_product_id: product.id,
       });
+
       break;
     }
-    case 'product.updated': {
+    case "product.updated": {
       const product = receivedEvent.data.object;
+
       await supabase
-        .from('plans')
+        .from("plans")
         .update({
           name: product.name,
           description: product.description,
         })
-        .eq('stripe_product_id', product.id);
+        .eq("stripe_product_id", product.id);
+
       break;
     }
-    case 'product.deleted': {
+    case "product.deleted": {
       const product = receivedEvent.data.object;
-      await supabase.from('plans').delete().eq('stripe_product_id', product.id);
+
+      await supabase.from("plans").delete().eq("stripe_product_id", product.id);
+
       break;
     }
     default:
